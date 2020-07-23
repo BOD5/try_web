e676e9de0c10e305edc4373619e223713c59dcc7
BOD5 <bod555@ukr.net>
2020-07-23 20:47:34 +0300

    File Backup: 2020-07-23_0847 PM_DESKTOP-R28G3O0

--- push_to_git.bat
+++ push_to_git.bat
@@ -1,3 +1,9 @@
-git add .
-git commit -m "update"
-git push
+@echo off
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
+For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
+Set hname=%COMPUTERNAME%
+cd %HOME%\my_dir
+git add --all
+git commit -m "File Backup: %mydate%_%mytime%_%hname%"
+git push  2>&1
+EXIT
